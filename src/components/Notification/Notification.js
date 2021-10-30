fd089464540840c91b25ef39037da69cf5451c8f
SlavikChuprin <chuprin.00@yandex.ru>
2021-10-30 12:19:13 +0300

    cretae tree components

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import s from './Notification.module.css'
+
+const Notification = ({
+message ="no info",
+} ) => {
+  return (
+    
+    <p className={s.notification}>{message}</p>
+   
+  );
+};
+
+Notification.propTypes = {
+message:PropTypes.string.isRequired,
+};
+
+export default Notification;

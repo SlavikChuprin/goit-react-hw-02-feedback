9de738c544bb8cddc471c5953a7fd1647cf25b42
SlavikChuprin <chuprin.00@yandex.ru>
2021-11-03 23:24:31 +0300

    fix bugs

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,32 +1,18 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Section.module.css';
-import Notification from '../Notification';
-import FeedbackOptions from '../FeedbackOptions';
-import Statistics from '../Statistics';
 
-const Section = ({title, onIncrement, state, onTotal, onPositivePercentage} ) => {
+const Section = ({ title, children }) => {
   return (
-    <div>
-    <section className={s.section}>{title[0]}
-    
-    <FeedbackOptions options={['good','neutral','bad']} onLeaveFeedback={onIncrement}/>
-    
+    <section className={s.section}>
+      {title}
+      {children}
     </section>
-   
-   <section className={s.section}>{onTotal(state) === 0 ? '' : title[1]}
-    {onTotal(state) === 0  ? <Notification message="No feedback given..."/> : <Statistics options= {state} onTotal={onTotal} onPositivePercentage={onPositivePercentage} />  }
-    </section>
-   </div>
   );
 };
 
 Section.propTypes = {
-  title:PropTypes.array.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  state: PropTypes.object.isRequired,
-  onTotal: PropTypes.func.isRequired,
-  onPositivePercentage:PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Section;
